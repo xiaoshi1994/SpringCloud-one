51a2dc9bb291ada1370963ef09a5343f6823acc1
xiaoshi1994 <2437766749@qq.com>
2018-11-21 11:36:10 +0800

    增加eureka集群

--- springCloud04-Eureka-9001/src/main/java/com/shi/eureka/Eureka9001.java
+++ springCloud04-Eureka-9001/src/main/java/com/shi/eureka/Eureka9001.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer //开启eureka服务器端注解 
-public class Eureka9000 {
+public class Eureka9001 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Eureka9000.class, args);
+		SpringApplication.run(Eureka9001.class, args);
 	}
 
 }
