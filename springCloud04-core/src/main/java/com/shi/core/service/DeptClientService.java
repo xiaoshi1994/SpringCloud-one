af89cce601c2a6f6a3abf587d6b0ed380f3bbf5e
xiaoshi1994 <2437766749@qq.com>
2018-12-04 17:06:19 +0800

    springCloud项目初始化阶段完成

--- springCloud04-core/src/main/java/com/shi/core/service/DeptClientService.java
+++ springCloud04-core/src/main/java/com/shi/core/service/DeptClientService.java
@@ -8,8 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.shi.core.model.Dept;
-
-@FeignClient(value = "SPRINGCLOUD04-PRODECT-8001") //指定为哪个微服务提供接口
+/**
+ * 让该接口实现熔断机制，让该接口在使用的中远程调用出错就调用 DeptClientServiceFallbackFactory 中指定的方法
+ * @author SHF
+ * @version 创建时间：2018年12月3日  上午10:36:25
+ */
+//@FeignClient(value = "SPRINGCLOUD04-PRODECT-8001")
+@FeignClient(name = "SPRINGCLOUD04-PRODECT-8001",fallbackFactory = DeptClientServiceFallbackFactory.class) //指定为哪个微服务提供接口
 public interface DeptClientService
 {
 	@RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
